df8619a7c2f684e84f7b5072cc2285da998135b6
Thomas-C-Voegeli <tcvoegeli@gmail.com>
2015-10-01 14:25:18 -0700

    Complete unstyled working todo list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+
+	def index
+		@tasks = Task.all.order(created_at: :asc)
+		@task = Task.new
+	end
+
+
+	def create
+		@tasks = Task.all
+		@task = Task.new(task_params)
+    	if @task.save
+      		redirect_to root_path 
+   		else
+   			@tasks = Task.all
+   			flash.now[:error] = "description must be at least 6 characters in length"
+      		render 'index'
+    	end
+	end
+
+	def update
+		task = Task.find(params[:id])
+		task.update(completed: !task.completed)
+		redirect_to root_path
+	end
+
+	def destroy
+		task = Task.find(params[:id])
+		task.destroy
+		redirect_to root_path
+	end
+
+	def active
+		@tasks = Task.active
+		@task = Task.new
+		render 'index'
+	end
+
+	def completed
+		@tasks = Task.completed
+		@task = Task.new
+		render 'index'
+	end
+
+	def destroy_completed
+		p "*" * 100
+		@tasks = Task.completed
+		@tasks.each do |task|
+			task.destroy
+		end
+		redirect_to root_path
+	end
+
+	private
+    def task_params
+      params.require(:task).permit(:description, :completed)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'tasks#index'
+  resources :tasks do
+    collection do
+      get 'active'
+      get 'completed'
+      post 'destroy_completed'
+    end
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
